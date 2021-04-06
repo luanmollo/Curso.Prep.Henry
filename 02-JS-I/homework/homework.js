4c065238947c452ff7794fa4eb8e139ee9f61a7f
luanmollo <luanmollo@gmail.com>
2021-04-06 12:47:08 -0300

    fixed operator or

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -252,19 +252,7 @@ function esVocal(letra){
   if (letra.length > 1){
     return "Dato incorrecto";
   }
-  else if (letra === "a") {
-    return "Es vocal";
-  }
-  else if (letra === "e"){
-    return "Es vocal";
-  }
-  else if (letra === "i"){
-    return "Es vocal";
-  }
-  else if (letra === "o"){
-    return "Es vocal";
-  }
-  else if (letra === "u"){
+  else if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u" ) {
     return "Es vocal";
   }
   else {
